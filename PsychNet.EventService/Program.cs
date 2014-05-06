c8ce359f8333ecb74e104605ed295d7fc0685408
will.master@gmail.com <will.master@gmail.com>
2014-05-06 11:27:51 +0100

    s

--- PsychNet.EventService/Program.cs
+++ PsychNet.EventService/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace PsychNet.EventService
 {
@@ -10,6 +11,39 @@ namespace PsychNet.EventService
     {
         static void Main(string[] args)
         {
+            HostFactory.Run(x =>                                 
+            {
+                x.Service<IService>(s =>                        
+                {
+                    s.ConstructUsing(name => new PsychNetEventService());   
+                    s.WhenStarted(tc => tc.Start());             
+                    s.WhenStopped(tc => tc.Stop());              
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("PsychNet.EventService");
+                x.SetDisplayName("PsychNet.EventService");
+                x.SetServiceName("PsychNet.EventService");                       
+            });       
+        }
+    }
+
+    public interface IService
+    {
+        void Start();
+        void Stop();
+    }
+
+    public class PsychNetEventService : IService
+    {
+        public void Start()
+        {
+            Console.WriteLine("Starting Service ...");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping the service ...");
         }
     }
 }
